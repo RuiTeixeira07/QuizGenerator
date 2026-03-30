994480d7dbca9a1ce3697425716f7b06dbb45a4d
Rui Teixeira <al2025155352@alunos.utad.pt>
2026-03-30 21:14:53 +0100

    Refactor 'File Extensions'.

--- file.py
+++ file.py
@@ -1,13 +1,15 @@
 import csv
 
+file_delimiter = ';'
+
 class File:
-    def __init__(self, file_path):
+    def __init__(self, file_path: str):
         self.file_path = file_path
 
-    def read_file(self):
+    def read_file(self) -> list[dict[str, str]]:
         try:
             with open(self.file_path, 'r') as file:
-                reader = csv.DictReader(file, delimiter=';')
+                reader = csv.DictReader(file, delimiter=file_delimiter)
                 return list(reader)
 
         except FileNotFoundError:
