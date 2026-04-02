52750b58a487ce2f9b091a511d586bc18928045e
Rui Teixeira <al2025155352@alunos.utad.pt>
2026-04-02 18:23:36 +0100

    Refactor 'File'.

--- QuizGenerator/file.py
+++ QuizGenerator/file.py
@@ -1,6 +1,7 @@
 import csv
 
 file_delimiter = ';'
+read_mode = "r"
 
 class File:
     def __init__(self: File, file_path: str):
@@ -8,7 +9,7 @@ class File:
 
     def read_file(self: File) -> list[dict[str, str]]:
         try:
-            with open(self.file_path, 'r') as file:
+            with open(self.file_path, read_mode) as file:
                 reader = csv.DictReader(file, delimiter=file_delimiter)
                 return list(reader)
 
