b77df361ca2d078110ed3594345ba2da5d587775
Rui Teixeira <al2025155352@alunos.utad.pt>
2026-04-03 15:35:43 +0100

    Address asking User for 'File Path'.

--- QuizGenerator/generator/format/format.py
+++ QuizGenerator/generator/format/format.py
@@ -1,3 +1,4 @@
+import os
 from xml.etree import ElementTree as ET
 
 indent_size = 4 * " "
@@ -21,3 +22,17 @@ class Format:
             formatted_text += open_paragraph_tag + paragraph + close_paragraph_tag
 
         return formatted_text
+
+    @staticmethod
+    def request_file_path(file_name: str, file_type: str, recommended_file_path: str, must_exist: bool = True) -> str:
+        while True:
+            file_path = input(f"Insert path to {file_name} (Recommended '{recommended_file_path}'): ").strip()
+
+            if not file_path:
+                file_path = recommended_file_path
+
+            if not must_exist and file_path.upper().endswith(file_type.upper()):
+                return file_path
+
+            if must_exist and file_path.upper().endswith(file_type.upper()) and os.path.isfile(file_path):
+                return file_path
--- QuizGenerator/main.py
+++ QuizGenerator/main.py
@@ -1,16 +1,21 @@
 from QuizGenerator.file.file import File
+from QuizGenerator.generator.format.format import Format
 from QuizGenerator.generator.generator import Generator
 from QuizGenerator.model.question import Question
 
-questions_data_path = "assets/QuestionsData.csv"
-generated_quiz_path = "quiz/Quiz.xml"
+questions_data_path_extension = ".CSV"
+generated_quiz_path_extension = ".XML"
+
+questions_data_path = f"assets/QuestionsData{questions_data_path_extension.upper()}"
+generated_quiz_path = f"quiz/Quiz{generated_quiz_path_extension.upper()}"
 
 class Main:
     @staticmethod
     def run() -> None:
-        questions_data = File(questions_data_path).read_file()
+        questions_data = File(Format.request_file_path("Questions Data", questions_data_path_extension, questions_data_path)).read_file()
         questions = Question.create_questions(questions_data)
-        Generator(generated_quiz_path).generate_quiz(questions)
+        Generator(Format.request_file_path("Generated Quiz", generated_quiz_path_extension, generated_quiz_path, False)).generate_quiz(questions)
+        print("Quiz Generated!")
 
 if __name__ == '__main__':
     main = Main()
