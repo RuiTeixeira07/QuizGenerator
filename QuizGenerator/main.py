fcf0e5e2009e52e5579f368ff5d0cfb37932ac52
Rui Teixeira <al2025155352@alunos.utad.pt>
2026-04-05 20:44:25 +0100

    Address 'Main' Formatting.

--- QuizGenerator/main.py
+++ QuizGenerator/main.py
@@ -12,9 +12,21 @@ generated_quiz_path = f"quiz/Quiz{generated_quiz_path_extension}"
 class Main:
     @staticmethod
     def run() -> None:
-        questions_data = File(Format.request_file_path("Questions Data", questions_data_path_extension, questions_data_path)).read_file()
+        questions_data = File(
+            Format.request_file_path(
+                "Questions Data",
+                questions_data_path_extension,
+                questions_data_path)).read_file()
+
         questions = Question.create_questions(questions_data)
-        Generator(Format.request_file_path("Generated Quiz", generated_quiz_path_extension, generated_quiz_path, False)).generate_quiz(questions)
+
+        Generator(
+            Format.request_file_path(
+                "Generated Quiz",
+                generated_quiz_path_extension,
+                generated_quiz_path,
+                False)).generate_quiz(questions)
+
         print("Quiz Generated!")
 
 if __name__ == '__main__':
